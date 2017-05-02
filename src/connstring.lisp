0d43f6cd9670ef1641e5fdc184a939ea5f59d7eb
David Vázquez Púa <davazp@gmail.com>
2017-05-02 23:36:55 +0200

    Add missing license header to src/connstring.lisp

--- src/connstring.lisp
+++ src/connstring.lisp
@@ -1,3 +1,23 @@
+;;; src/connstring.lisp ---
+
+;; Copyright (c) 2017 David Vázquez Púa
+
+;; This file is part of pgsnipe.
+
+;; pgsnipe is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; pgsnipe is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with pgsnipe.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (defpackage :pgsnipe/connstring
   (:use :common-lisp :quri :pgsnipe/utils)
   (:export #:parse))
